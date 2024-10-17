b77eddfb1e2272c6e91ee0710ba679c346738667
carolcortes <caroline.ocortes@gmail.com>
2024-10-17 23:36:38 +0100

    fix ex08

--- ex08/ft_is_negative.c
+++ ex08/ft_is_negative.c
@@ -6,9 +6,11 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:49:34 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/10/14 15:55:24 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/10/17 23:36:03 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	ft_putchar(char c);
 
 void	ft_is_negative(int n)
 {
