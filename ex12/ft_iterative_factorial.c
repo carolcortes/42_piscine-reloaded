ae490a9a3477cb19ce3d2b5b6d82885dac63fa62
Caroline De Oliveira Cortes <cade-oli@c1r4s4.42porto.com>
2024-10-18 12:20:18 +0100

    fix ex12

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:12:40 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/10/14 16:46:45 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/10/18 12:19:49 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@ int	ft_iterative_factorial(int nb)
 	int	result;
 
 	result = 1;
-	while (nb >= 1)
+	if (nb < 0)
+		return (0);
+	while (nb >= 1 && nb != 0)
 	{
 		result *= nb;
 		nb--;
