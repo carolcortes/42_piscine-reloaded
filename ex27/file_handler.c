d78a4c82b41c850dcb9096b523ed06d4999974c4
Caroline De Oliveira Cortes <cade-oli@c1r4s4.42porto.com>
2024-10-18 13:34:45 +0100

    fix ex27

--- ex27/file_handler.c
+++ ex27/file_handler.c
@@ -6,7 +6,7 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 10:14:05 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/10/18 12:02:36 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/10/18 13:17:57 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_display_file(char *filename)
 	int		file;
 	char	c;
 
-	file = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY, 0);
 	if (file == -1)
-		write(1, "Cannot read file.", 17);
+		write(1, "Cannot read file.\n", 18);
 	else
 	{
-		while (read(file, &c, 1))
+		while (read(file, &c, 1) == 1)
 			write(1, &c, 1);
 		close(file);
 	}
--- ex27/main.c
+++ ex27/main.c
@@ -6,7 +6,7 @@
 /*   By: cade-oli <cade-oli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 10:13:58 by cade-oli          #+#    #+#             */
-/*   Updated: 2024/10/18 12:10:03 by cade-oli         ###   ########.fr       */
+/*   Updated: 2024/10/18 13:15:47 by cade-oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 1)
-		write(1, "File name missing.", 18);
+	if (argc < 2)
+		write(1, "File name missing.\n", 19);
 	else if (argc > 2)
-		write(1, "Too many arguments.", 19);
+		write(1, "Too many arguments.\n", 20);
 	else
 		ft_display_file(argv[1]);
 	return (0);
